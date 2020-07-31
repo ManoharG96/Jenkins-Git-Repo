af0afc546040929f1c4d44d39629fd67f0fc79a8
ManoharG96 <manohargreddy96@gmail.com>
2020-07-31 10:03:36 +0530

    Commit to branch

---
+++ Sample.java
@@ -0,0 +1,10 @@
+public class Sample{
+public static void main(String[] arg){
+System.out.println("Welcome to Branch Git");
+for (int i=0; i<10; i++){
+System.out.println("Branch "+i);
+}
+System.out.println("Committing For to Branch");
+}
+
+}
